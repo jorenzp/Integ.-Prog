893ee7478191e35ec39829d675755327ec915a25
Jorenz Pablo <jorenzpablo@gmail.com>
2024-03-10 18:11:43 +0800

    commit

--- index.js
+++ index.js
@@ -6,10 +6,3 @@ function signInWithGoogle() {
       window.location.href = "patientmenu.html";
     });
   }
-
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        console.log('User signed out.');
-    });
-}
