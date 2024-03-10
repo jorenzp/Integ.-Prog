302baff9f6d3d60bc82f3b5b3a6b00b6f054f802
Jorenz Pablo <jorenzpablo@gmail.com>
2024-03-10 13:56:30 +0800

    commit

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    $("#name").text(profile.getName());
-    $("#email").text(profile.getEmail());
-    $("#image").attr('src', profile.getImageUrl());
-    $(".data").css("display", "block");
-    $(".g-signin2").css("display", "none");
-}
-
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      alert('User signed out.');
-      $(".g-signin2").css("display", "block");
-      $(".data").css("display", "none");
-    });
+const button = document.getElementById('signout_button');
+button.onclick = () => {
+  google.accounts.id.disableAutoSelect();
 }
