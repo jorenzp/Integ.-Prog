ec74be66abdb61182b9322872eb743c8f751bae2
Jorenz Pablo <jorenzpablo@gmail.com>
2024-03-10 18:34:40 +0800

    commit

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-function signInWithGoogle() {
-    // Trigger the Google sign-in process
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signIn().then(function(googleUser) {
-      var profile = googleUser.getBasicProfile();
-      window.location.href = "patientmenu.html";
+// Function to initialize Google Sign-In
+function init() {
+    gapi.load('auth2', function() {
+        var auth2 = gapi.auth2.init({
+            client_id: '509346360505-3np7pn849nskuercs5dqad9sgqooksod.apps.googleusercontent.com',
+            // Specify your login endpoint
+            login_hint: 'https://jorenzp.github.io/Integ.-Prog/login.html',
+            // Add any additional options here if needed
+        });
+
+        // Get reference to your sign-in button
+        var signInButton = document.getElementById('g_id_signin');
+
+        // Attach click event listener to sign-in button
+        signInButton.addEventListener('click', function() {
+            // Trigger Google Sign-In process
+            auth2.signIn().then(onSuccess).catch(onFailure);
+        });
     });
-  }
+}
+
+// Function to handle successful sign-in
+function onSuccess(user) {
+    console.log('Signed in as ' + user.getBasicProfile().getName());
+    window.location.href = "patientmenu.html";
+}
+
+// Function to handle sign-in failure
+function onFailure(error) {
+    console.error('Sign-in failed:', error);
+}
